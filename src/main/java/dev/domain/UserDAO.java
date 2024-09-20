ab33f8ed1da4b30c4f4e9d83eb48d851421bbddb
82177 <8217759juan@naver.com>
2024-09-20 22:03:24 +0900

    feat: add UserDAO

---
+++ src/main/java/dev/domain/UserDAO.java
@@ -0,0 +1,23 @@
+package dev.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+
+public class UserDAO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id = UUID.randomUUID().toString();
+    private String userName;
+    private String passWord;
+}
